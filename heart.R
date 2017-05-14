35aafa0ac6a73a4c9e733bfe86fdac3367e1ab36
dsm14 <moh.sar1412@gmail.com>
2017-05-14 22:29:55 +0530

    Update heart.R

--- heart.R
+++ heart.R
@@ -20,28 +20,38 @@ colSums(is.na(ht))
 #train<-ht[index,]
 #test<-ht[-index,]
 
+##Sampling##
+set.seed(200)
+index<-sample(nrow(ht), 0.70 *nrow(ht), replace=F)
+train<-ht[index,]
+test<-ht[-index,]
+
 ##Logistic Regression Model Building##
 #Iteration 1
-mod<- glm(DV~., data= ht[,-1], family = "binomial")
+mod<- glm(DV~., data= train[,-1], family = "binomial")
 summary(mod)
 
 ##Iteration2
 coef(summary(mod))[, "Pr(>|z|)"]< 0.1
 
-mod<-glm(data = ht, DV ~ Sex + cp + trestbps + thalach + ca + thal + slope, family = "binomial")
+mod<-glm(data = train, DV ~ Sex + cp + trestbps + thalach + ca + thal + slope, family = "binomial")
 summary(mod)
 
+## Checking multicollinearity##
+#library(car)
+#vif(mod)
+
 ##AUC##
-pred_test<-predict(mod, type = "response", newdata = ht)
+pred_test<-predict(mod, type = "response", newdata = train)
 library(ROCR)
-p1<-prediction(pred_test, ht$DV)
+p1<-prediction(pred_test, train$DV)
 auc<-performance(p1, "auc")
 auc<-unlist(slot(auc, "y.values"))
 auc
 
 
 ##Classification Matrix= 0.6##
-p1<-prediction(pred_test, ht$DV)
+p1<-prediction(pred_test, train$DV)
 p2<-performance(p1, "tpr", "fpr")
 plot(p2, colorize=TRUE, print.cutoffs.at=seq(0,1,by=0.1), text.adj=c(-0.2,1.7))
 
@@ -51,19 +61,19 @@ pred_test_label<-ifelse(pred_test>=0.6, "High Risk", "No Risk")
 head(pred_test_label, 20)
 
 ##Confusion Matrix##
-table(pred_test_label, ht$DV)
+table(pred_test_label, train$DV)
 
 
 ##Model interpretation and recommendation ##
 
-##Scoring ##
+##Scoring on test dataset##
 library(gains)
-gains(ht$DV, predict(mod, type="response", newdata=ht), groups = 10)
+gains(test$DV, predict(mod, type="response", newdata=test), groups = 10)
 
-ht$prob<-predict(mod, type = "response", newdata = ht)
-quantile(ht$prob, prob=c(0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.80,0.90,1))
+test$prob<-predict(mod, type = "response", newdata = test)
+quantile(test$prob, prob=c(0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.80,0.90,1))
 
-targeted<-ht[ht$prob>=0.89629237 & ht$prob<=0.99650896, ]
+targeted<-test[test$prob>=0.89629237 & test$prob<=0.99650896, ]
 targeted
 
 library(dplyr)
@@ -72,5 +82,3 @@ targeted
 
 write.xlsx(targeted, "C:\\Users\\madhurima\\Downloads\\targeted.xlsx")
 
-##library(car)
-#vif(mod)
